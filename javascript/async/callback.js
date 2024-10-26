74d649e6a79f8f8055443127225a227d76774dac
Atharva0506 <atharvan.coder@gmail.com>
2024-10-26 18:21:40 +0530

    fs promises

--- javascript/async/callback.js
+++ javascript/async/callback.js
@@ -1,21 +1,37 @@
-function sum(x,y){
-    return x+y
+const fs = require('fs').promises
+
+//  Functional arguments 
+
+function sum(x, y) {
+    return x + y
 }
 
-function sub(x,y){
-    return x-y
+function sub(x, y) {
+    return x - y
 }
 
-function mul(x,y){
-    return x*y
+function mul(x, y) {
+    return x * y
 }
 
-function div(x,y){
-    return x/y
+function div(x, y) {
+    return x / y
 }
 
-function doAll(x=0,y=0,op=sum){
-    return op(x,y)
+function doAll(x = 0, y = 0, op = sum) {
+    return op(x, y)
 }
 
-console.log(doAll(15,41,mul))
+console.log(doAll(15, 41, mul))
+
+async function readData(fileName) {
+    try {
+        const data = await fs.readFile(fileName, "utf-8");
+        return data;
+    } catch (err) {
+        return err;
+    }
+}
+
+
+readData('a.txt').then((data) => { console.log(data) })
