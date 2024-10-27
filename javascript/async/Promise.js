9d104c85efff65843425b280f6b3476019612a89
Atharva0506 <atharvan.coder@gmail.com>
2024-10-27 11:24:11 +0530

    own promise

--- javascript/async/Promise.js
+++ javascript/async/Promise.js
@@ -1,9 +1,67 @@
-function setTimeoutPromisified(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+const fs = require("fs");
+
+// function setTimeoutPromisified(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms))
+// }
+
+// function callback(x=5000) {
+//     console.log(`${x} seconds have passed!`)
+// }
+// let ms = 5000
+// setTimeoutPromisified(ms).then(callback)
+
+// promise is supposed to return somthing
+
+// function random(res){
+//     setTimeout(res,3000)
+// }
+// let p =new Promise(random)
+
+// function callback(){
+//     console.log("Hello there!!!")
+// }
+// console.log(p)
+
+// p.then(callback)
+
+// function dataClean(resolve){
+//     fs.readFile('a.txt','utf-8',(err,data)=>{
+//         resolve(data)
+//     })
+// }
+
+// function cleanFile(){
+//     return new Promise(dataClean)
+// }
+
+// const p = cleanFile()
+
+// p.then((x)=>{console.log(x)})
+
+class MyPromise {
+  constructor(fn) {
+    this.resolve = () => {};
+    const afterTaskIsDone = ()=> {
+      this.resolve();
+    }
+    fn(afterTaskIsDone);
+  }
+  then(callback) {
+    this.resolve = callback;
+  }
 }
 
-function callback(x=5000) {
-    console.log(`${x} seconds have passed!`)
+function asyncTask() {
+  return new MyPromise((resolve) => {
+    setTimeout(() => {
+      console.log("set timeout done");
+      resolve();
+    }, 3000);
+  });
 }
-let ms = 5000
-setTimeoutPromisified(ms).then(callback)
+
+const myP = asyncTask();
+
+myP.then(() => {
+  console.log("task completed");
+});
