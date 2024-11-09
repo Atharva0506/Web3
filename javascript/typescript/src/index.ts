96a03343dd7292ca0f77ac10e683c08882e972be
Atharva0506 <atharvan.coder@gmail.com>
2024-11-09 21:38:12 +0530

    interface

--- javascript/typescript/src/index.ts
+++ javascript/typescript/src/index.ts
@@ -1,33 +1,47 @@
-
-function greet(name:string){
-    console.log(`hello ${name}`);
+function greet(name: string) {
+  console.log(`hello ${name}`);
 }
 
-greet("Luffy")
+greet("Luffy");
 
-function sum(a:number,b:number) : number{
-    return a+b
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(35, 34));
+
+function isLegal(a: number): boolean {
+  if (a > 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isLegal(19));
+
+function delayCall(fn: () => void, t: number) {
+  setTimeout(fn, t);
+}
+delayCall(() => {
+  console.log("Delayed call");
+}, 2000);
+
+interface User {
+  firstname: string;
+  lastname: string;
+  email: string;
+  age: number;
 }
 
 
-console.log(sum(35,34))
-
-
-function isLegal(a:number):boolean{
-    if(a>18){
-        return true
-    }
-    else{
-        return false
-    }
+function isLegal2(user:User){
+    return (user.age > 18 ? true : false)
 }
 
-console.log(isLegal(19))
-
-function delayCall(fn: ()=>void,t:number){
-    setTimeout(fn,t)
-}
-
-console.log(delayCall(()=>{
-    console.log("Delayed call")
-},2000))
+console.log(isLegal2({
+    firstname:"Lufyy",
+    lastname:"D",
+    email:"luffy@onepiece.com",
+    age:20
+}))
