a02f170599156dc18c36fac050d790eb4438f085
Atharva0506 <atharvan.coder@gmail.com>
2024-11-10 21:44:36 +0530

    array types

--- javascript/typescript/src/index.ts
+++ javascript/typescript/src/index.ts
@@ -32,16 +32,121 @@ interface User {
   lastname: string;
   email: string;
   age: number;
+  addres?: {
+    city: string;
+    country: string;
+    pincode: number;
+  };
 }
 
-
-function isLegal2(user:User){
-    return (user.age > 18 ? true : false)
+function isLegal2(user: User) {
+  return user.age > 18 ? true : false;
 }
 
-console.log(isLegal2({
-    firstname:"Lufyy",
-    lastname:"D",
-    email:"luffy@onepiece.com",
-    age:20
-}))
+console.log(
+  isLegal2({
+    firstname: "Lufyy",
+    lastname: "D",
+    email: "luffy@onepiece.com",
+    age: 20,
+  })
+);
+
+type S = string | number;
+
+function add(a: S, b: S) {
+  return `${a}${b}`;
+}
+
+console.log(add(12, 23));
+console.log(add("Monkey .D ", "Luffy"));
+
+let x: string[] = ["Monkey", " .D ", "Luffy"];
+
+console.log(x.join());
+
+interface People {
+  name: string;
+  age: number;
+  greet?: () => string;
+}
+
+let p: People = {
+  name: "Luffy",
+  age: 20,
+  greet: () => {
+    return "hello";
+  },
+};
+
+console.log(p.greet?.());
+
+class MyClass implements People {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+const myClass = new MyClass("lufyy", 21);
+
+console.log(myClass.age);
+
+interface Admin {
+  name: string;
+  permission: string;
+}
+
+interface User2 {
+  name: string;
+  age: number;
+}
+
+type UserOrAdmin = User2 | Admin;
+
+function greet2(user: UserOrAdmin) {
+  console.log(user.name);
+}
+
+function get_max(arr: number[]): number {
+  let maxValue: number = 0;
+  arr.forEach((data) => {
+    if (data > maxValue) {
+      maxValue = data;
+    }
+  });
+  return maxValue;
+}
+
+console.log(get_max([12, 11, 4, 22, 3, 44, 10, 10]));
+
+interface User3 {
+  firstname: string;
+  lastname: string;
+  age: number;
+}
+
+function getMaxAge(user: User3[]) {
+  return user.filter((x) => x.age > 18);
+}
+
+console.log(
+  getMaxAge([
+    {
+      firstname: "x",
+      lastname: "y",
+      age: 17,
+    },
+    {
+      firstname: "a",
+      lastname: "b",
+      age: 19,
+    },
+    {
+      firstname: "q",
+      lastname: "w",
+      age: 22,
+    },
+  ])
+);
