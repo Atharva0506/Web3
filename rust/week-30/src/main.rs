bbcea6f2886cfe6550cbb8998a7864e1851dd9da
Atharva0506 <atharvan.coder@gmail.com>
2025-03-07 20:38:48 +0530

    Updated .gitignore and cleaned up unnecessary files

---
+++ rust/week-30/src/main.rs
@@ -0,0 +1,23 @@
+use serde::{Serialize, Deserialize};
+use serde_json::{self, Value};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+fn main() {
+    let person = Person {
+        name: String::from("John Doe"),
+        age: 30,
+    };
+
+    // Serialize to JSON
+    let json_str = serde_json::to_string(&person).unwrap();
+    println!("Serialized JSON: {}", json_str);
+
+    // Deserialize from JSON
+    let deserialized_person: Person = serde_json::from_str(&json_str).unwrap();
+    println!("Deserialized Person: {:?}", deserialized_person);
+}
